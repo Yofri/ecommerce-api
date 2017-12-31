278f1aa8e6769d321dc26827910c40e38289b1ca
Yofriadi Yahya <yofriadiyahya@gmail.com>
2017-12-31 18:02:57 +0700

    Fix port

--- src/index.js
+++ src/index.js
@@ -6,13 +6,12 @@ import {user, store, transaction} from './routes'
 
 dotenv.config()
 const app = express()
-const port = process.env.PORT || 3000
 
 app.use(cors())
 app.use('/user', user)
 app.use('/store', store)
 app.use('/transaction', transaction)
 
-db.on('error', () => console.log(`Error connecting to database`))
-db.once('open', () => console.log(`Connected to database`))
-app.listen(port, () => console.log(`Express listening on port ${port}`))
+db.on('error', () => console.log('Error connecting to database'))
+db.once('open', () => console.log('Connected to database'))
+app.listen(3000, () => console.log('Express listening on port 3000'))
